ef347efd9642c1eacea507ba5eb5db81bad8e304
Tales <talescavalcanti06@gmail.com>
2026-08-17 18:54:58 -0300

    feat: adiciona exercício 4

---
+++ unidade-01/exercicio04.c
@@ -0,0 +1,27 @@
+/*Código fornecido:
+#include <stdio.h>
+#include <stdlib.h>;
+int Main{}
+(
+printf( Existem %d semanas no ano.,52);
+cout << endl;
+system("PAUSE");
+return 0;
+)
+*/
+
+/*Código corrigido*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int semanasAno = 52;
+
+    printf("Existem %d semanas no ano. ", semanasAno);
+    system("PAUSE");
+
+    return 0;
+}
+
