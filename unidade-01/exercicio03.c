53f0b1a8c3566a08589ad21d76efede49bd5c1c6
Tales <talescavalcanti06@gmail.com>
2026-08-17 18:47:52 -0300

    feat: adiciona exercicio 3

---
+++ unidade-01/exercicio03.c
@@ -0,0 +1,17 @@
+/*Esse programa mostra o uso dos comentários na linguagem C*/
+
+#include <stdio.h> /*Para a função printf()*/
+#include <stdlib.h> /*Para system()*/
+
+
+int main() /*Função main*/
+{ /*Início do corpo da função*/
+
+    int idade = 19; /*Declaro a variável idade com valor atribuido 19*/
+    printf("Oi, tenho %d anos. ", idade); /*Chama a função printf formatando a variavel idade.*/
+    system("PAUSE"); /*Chama a função system()*/
+
+    return 0;
+
+
+}/*Fim do corpo da função*/
