1be1e8ffab2a4a51b0991e2c0536716a29b2b8f4
Tales <talescavalcanti06@gmail.com>
2026-08-17 18:27:23 -0300

    feat: segundo exercicio da lista de exercicios

---
+++ unidade-01/segExercicio.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    float PI = 3.1415;
+    printf("Valor de PI com 3 casas decimais: %.3f. ", PI);
+    system("PAUSE");
+
+    return 0;
+}
