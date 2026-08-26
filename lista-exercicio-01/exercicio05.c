d12dec89b7f924ab750d0e07ccfd2d41150d3136
Tales <talescavalcanti06@gmail.com>
2026-08-26 18:28:27 -0300

    feat: adiciona exercicio 5 e 6

---
+++ lista-exercicio-01/exercicio05.c
@@ -0,0 +1,20 @@
+/*Código fornecido*/
+/*main()
+{
+printf("Linguagem C");
+system("pause");
+}*/
+
+/*Código corrigido*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    printf("Linguagem C. ");
+    system("PAUSE");
+    return 0;
+
+}
+
